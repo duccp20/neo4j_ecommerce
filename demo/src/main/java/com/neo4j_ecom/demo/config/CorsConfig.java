7e365960a6074c75ee34a3f2787a0ad17fc01048
phuoc <duccp20@gmail.com>
2024-09-25 23:38:11 +0700

    allow all request using api , config cors

--- demo/src/main/java/com/neo4j_ecom/demo/config/CorsConfig.java
+++ demo/src/main/java/com/neo4j_ecom/demo/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("/**")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true)
